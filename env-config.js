6e7fbd7c86b159d78d69b4563b8e94ca2dd0c9b4
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-12-05 20:46:50 -0800

    Use cloudinary for assets.

--- env-config.js
+++ env-config.js
@@ -1,12 +1,11 @@
 // this file exports a bunch of replacements
 // that are made across the source-code
 
-const { NODE_ENV } = process.env
-
 module.exports = {
   VERSION: require('./package').version,
-  'process.env.NODE_ENV': NODE_ENV,
-  IMAGE_ASSETS_URL: '/static',
-  VIDEO_ASSETS_URL: '/static',
-  RAW_ASSETS_URL: '/static'
+  'process.env.NODE_ENV': process.env.NODE_ENV,
+  // We need to introduce a better way to deal with this for GH based PRs
+  IMAGE_ASSETS_URL: 'https://res.cloudinary.com/zeit-inc/image/upload/front',
+  VIDEO_ASSETS_URL: 'https://res.cloudinary.com/zeit-inc/video/upload/front',
+  RAW_ASSETS_URL: 'https://res.cloudinary.com/zeit-inc/raw/upload/front'
 }
